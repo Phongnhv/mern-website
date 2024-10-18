226d2393678de3c03f0d9cdb0230115f6e3636f1
minh <enoeul.03@gmail.com>
2024-10-18 13:26:39 +0700

    minh commit

--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import useRouter from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-    console.log('Connected to db');
-}).catch((err) =>{
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.use("/api/user", useRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        require: true,
-        unique: true
+      type: String,
+      require: true,
+      unique: true,
     },
     email: {
-        type: String,
-        require: true,
-        unique: true
+      type: String,
+      require: true,
+      unique: true,
     },
     password: {
-        type: String,
-        require: true
-    }
-},{timestamps: true});
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
