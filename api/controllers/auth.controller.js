c7615c647113f118b3e0b8e0957238a4f513f773
Phongnhv <phongnhvp@gmail.com>
2024-10-23 20:27:16 +0700

    KAN-3 define SignIn Routes and add Middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,31 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/errorHandler.js';
+import jwt from 'jsonwebtoken'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created successfully!");
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const existedUser = await User.findOne({ email });
+    if (!existedUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, existedUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: existedUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = existedUser._doc;
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
