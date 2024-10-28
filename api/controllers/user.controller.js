ba7497ec1df70b22255489d71e21474b9cc2e6c8
minh <enoeul.03@gmail.com>
2024-10-28 15:35:14 +0700

    KAN_5 complete api delete user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,6 +31,18 @@ export const updateUser = async (req, res, next) => {
 
     res.status(200).json(rest);
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only delete own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json("user have been deleted");
+  } catch (error) {
+    next(error);
   }
 };
