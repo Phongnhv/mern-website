a9a154f162c4b0400a9ca9003400573b06d1b3de
Misakichan2k2 <shiro.kuro0523@gmail.com>
2024-11-29 16:20:03 +0700

    KAN-5 Create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from './pages/UpdateListing';
+import UpdateListing from "./pages/UpdateListing";
+import Search from "./pages/Search";
 
 export default function App() {
   return (
@@ -18,11 +19,12 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route
-            path='/update-listing/:listingId'
+            path="/update-listing/:listingId"
             element={<UpdateListing />}
           />
         </Route>
