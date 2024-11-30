e4a6bf0013e43cae898e9b4471531487e9b27bf9
Phongnhvp <phongnhvp@gmail.com>
2024-11-30 21:07:29 +0700

    local commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  apiKey: "AIzaSyBbVwUjY40Zpov46aT1_RxiwIi6X2v-kTw",
   authDomain: "projectii-4f4f0.firebaseapp.com",
   projectId: "projectii-4f4f0",
   storageBucket: "projectii-4f4f0.appspot.com",
   messagingSenderId: "819125072776",
   appId: "1:819125072776:web:890aa66d0db76448ec003d",
-  measurementId: "G-J49XGJJP2X",
+  measurementId: "G-J49XGJJP2X"
 };
 
 // Initialize Firebase
