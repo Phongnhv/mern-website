6b27d478fb730cc2ce2dbe55995126e169d95ba6
Phongnhvp <phongnhvp@gmail.com>
2024-12-17 17:09:29 +0700

    final changes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -46,7 +46,7 @@ export default function SignUp() {
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
       </form>
       <div className='flex flex-col mt-4 '>
-      <OAuth />
+      
       </div>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
